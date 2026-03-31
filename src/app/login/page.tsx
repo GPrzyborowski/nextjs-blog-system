b27852cc538ad3d7fcc15de46137f700c4a522aa
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-31 21:35:32 +0200

    add login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,11 @@
 import UpperContainer from '@/components/UpperContainer'
+import LoginForm from '@/components/LoginForm'
 
 export default function Login() {
     return (
         <main className="flex-1 bg-neutral-50">
             <UpperContainer title="Log in to your account" />
-
+            <LoginForm />
         </main>
     )
 }
